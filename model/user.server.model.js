7b6ecbb0096726756f59267d077bea00b5682483
Deadpool1994 <mhtrami@gmail.com>
2016-03-29 00:33:36 +0530

    Backend of Akaunto1.0

--- model/user.server.model.js
+++ model/user.server.model.js
@@ -20,5 +20,55 @@ userSchema = new Schema({
     email: {
         type: mongoose.SchemaTypes.Email,
         required: true
-    }
+    },
+    totalBalance: {
+        type: Number,
+        default: 0
+    },
+    groups: [{type: Schema.Types.ObjectId, ref: 'Group'}],
+    transactions: [{type: Schema.Types.ObjectId, ref: 'Transaction'}]
 });
+
+var groupSchema = new Schema({
+   groupname: {
+       type: String,
+       required: true
+   },
+    balance: {
+        type: Number,
+        default: 0
+    },
+    users: [{type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+var transactionSchema = new Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    transaction_date: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    user_to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    User_from: {
+        type: Schema.types.ObjectId,
+        ref: 'User'
+    }
+
+});
+
+var User = mongoose.model('User', userSchema);
+
+var Group = mongoose.model('Group', groupSchema);
+
+var Transaction = mongoose.model('Transaction', transactionSchema);
